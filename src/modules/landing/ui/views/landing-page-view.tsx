b8f28b7c7fc6c77e6b6d35fccbbd7cac90f420b1
patryk <paniczgg@gmail.com>
2026-01-07 15:16:55 +0100

    landing-page

---
+++ src/modules/landing/ui/views/landing-page-view.tsx
@@ -0,0 +1,11 @@
+import { HeroHeader } from "@/modules/landing/ui/components/header"
+import HeroSection from "@/modules/landing/ui/components/hero-section"
+
+export const LandingPageView = () => { 
+  return (
+    <div>
+      <HeroHeader />
+      <HeroSection />
+    </div>
+  )
+}
