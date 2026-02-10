cd316eec1f8f03782ae1b05dbc25cc6f8de50dc2
patryk <paniczgg@gmail.com>
2026-02-10 15:12:43 +0100

    message ui

---
+++ src/modules/dashboard/ui/components/channel-message-list.tsx
@@ -0,0 +1,40 @@
+import { MessageItem } from "@/modules/dashboard/ui/components/message/MessageItem"
+
+const messages = [ 
+  { 
+    id: 1,
+    message: "fdafafasfaf",
+    date: new Date(),
+    avatar: "https://avatars.githubusercontent.com/u/124599?v=4",
+    userName: "Panicz"
+  },
+  { 
+    id: 2,
+    message: "fdadafafasfaf",
+    date: new Date(),
+    avatar: "https://avatars.githubusercontent.com/u/124599?v=4",
+    userName: "Panicz"
+  },
+  { 
+    id: 3,
+    message: "fdafadafasfaf",
+    date: new Date(),
+    avatar: "https://avatars.githubusercontent.com/u/124599?v=4",
+    userName: "Panicz"
+  }
+]
+
+export const ChannelMessageList = () => { 
+  return (
+    <div className="relative h-full">
+      <div className="h-full overflow-y-auto px-4">
+        {messages.map((message) => ( 
+          <MessageItem
+            key={message.id}
+            {...message}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
