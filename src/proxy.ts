ecf84cbf3b921773f637c403d9f25eae9e717ed1
patryk <paniczgg@gmail.com>
2026-03-08 11:48:07 +0100

    middleware optimalization

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,41 +1,54 @@
-import arcjet, { createMiddleware, detectBot }  from '@arcjet/next'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest, NextResponse } from 'next/server'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextRequest, NextResponse, NextProxy } from 'next/server';
+import arcjet, { createMiddleware, detectBot } from '@arcjet/next';
+import { withAuth } from '@kinde-oss/kinde-auth-nextjs/server';
 
-const aj = arcjet({ 
-  key: process.env.ARCJET_KEY!,
-  rules: [
-    detectBot({ 
-      mode: "LIVE",
-      allow: [
-        "CATEGORY:SEARCH_ENGINE",
-        "CATEGORY:PREVIEW",
-        "CATEGORY:MONITOR",
-        "CATEGORY:WEBHOOK",
-      ]
-    })
-  ]
-})
+const aj = arcjet({
+	key: process.env.ARCJET_KEY!,
+	rules: [
+		detectBot({
+			mode: 'LIVE',
+			allow: [
+				'CATEGORY:SEARCH_ENGINE',
+				'CATEGORY:PREVIEW',
+				'CATEGORY:MONITOR',
+				'CATEGORY:WEBHOOK',
+			],
+		}),
+	],
+});
 
-async function existingMiddleware(req: NextRequest) { 
-  const { getClaim } = getKindeServerSession()
-  const orgCode = await getClaim("org_code")
+async function existingMiddleware(req: NextRequest) {
+	const anyReq = req as {
+		nextUrl: NextRequest['nextUrl'];
+		kindeAuth?: { token?: any; user?: any };
+	};
 
-  const url = req.nextUrl
+	const url = req.nextUrl;
 
-  if (
-    url.pathname.startsWith('/workspace') && !url.pathname.includes(orgCode?.value || "")
-  ) { 
-    url.pathname = `/workspace/${orgCode?.value}`
+	const orgCode =
+		anyReq.kindeAuth?.user?.org_code ||
+		anyReq.kindeAuth?.token?.org_code ||
+		anyReq.kindeAuth?.token?.claims?.org_code;
 
-    return NextResponse.redirect(url)
-  }
+	if (
+		url.pathname.startsWith('/workspace') &&
+		!url.pathname.includes(orgCode || '')
+	) {
+		url.pathname = `/workspace/${orgCode}`;
+		return NextResponse.redirect(url);
+	}
 
-  return NextResponse.next()
+	return NextResponse.next();
 }
 
-export default createMiddleware(aj, existingMiddleware)
+export default createMiddleware(
+	aj,
+	withAuth(existingMiddleware, {
+		publicPaths: ['/'],
+  }) as NextProxy
+);
 
-export const config = { 
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|/rpc).*)"],
-}
+export const config = {
+	matcher: ['/((?!_next/static|_next/image|favicon.ico|/rpc).*)'],
+};
